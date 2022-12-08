24f0be90a3c4049de2c1d2f24c9576968313873e
willCodesInBinary <112642024+WCoder17@users.noreply.github.com>
2022-12-08 09:59:26 -0700

    Solid Progress
    Almost finished the Sing up, no work done for Log in yet.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,65 @@
-// USER LOGIN / SIGNUP
+// User Login
 
-// HTML VARIABLES
-let signInBtn = document.getElementById('sign-in-btn');
-let signUpBtn = document.getElementById('sign-up-btn');
+// Elements
+let sinBtnEl = document.getElementById("sign-up-btn");
+let logBtnEl = document.getElementById("sign-ip-btn");
+let sinNamEl = document.getElementById("sign-name");
+let sinPasEl = document.getElementById("sign-pass");
+let conPasEl = document.getElementById("sign-conpass");
+let logNamEl = document.getElementById("log-name");
+let logPasEl = document.getElementById("log-pass");
 
-// SIGN UP BTN CLICKED
-signUpBtn.addEventListener('click', signUpHandler);
+// Button Event Listeners
+sinBtnEl.addEventListener("click", sinBtnHandler);
+//logBtnEl.addEventListener("click", logBtnHandler);
 
-function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
+let members = loadMembers();
+
+function sinBtnHandler() {
+  console.log("Sign In");
+
+  saveMembers();
+}
+function logBtnHandler() {}
+
+// Main Functions
+function addUser() {
+  let name = sinNamEl.value;
+  let password = sinPasEl.value;
+  members.push(newMember(name, password));
+  alert(`Hello ${name}. Welcome to the club`);
 }
 
-// SIGN IN BTN CLICKED
-signInBtn.addEventListener('click', signInHandler);
+function checkUser() {
+  let nameC = sinNamEl.value;
+  for (let i = 0; i < members.length; i++) {
+    if (members[i].name === nameC) {
+      alert("Username is already taken");
+    } else {
+      addUser();
+    }
+  }
+}
 
-function signInHandler() {
-  console.log('Sign In Btn Clicked');
+function checkPass() {
+  let passC = sinPasEl.value;
+  let cPass = conPasEl.value;
+  if (passC !== cPass) {
+    alert("Passwords do not match");
+  } else {
+    checkUser();
+  }
+}
+function newMember(memberName, memberPassword) {
+  return { name: memberName, password: memberPassword, completed: "" };
+}
+// Local Save
+
+function saveMembers() {
+  localStorage.setItem("members", JSON.stringify(members));
+}
+
+function loadMembers() {
+  let memberStr = localStorage.getItem("members");
+  return JSON.parse(memberStr) ?? [];
 }
